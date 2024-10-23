eb5db0363e27e47d90795c9fc672a67b3815712a
Mathew <115046381+MatHartley@users.noreply.github.com>
2024-10-23 13:38:46 +1100

    Imported Fonts
    Imported and Coloured Fonts
    Started the Main Menu
    Started the Credits

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+    void LoadGarage()
+    {
+
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
